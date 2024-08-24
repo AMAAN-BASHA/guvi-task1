e32130a192b29646bf2c561504e08fac3a9fff35
AMAAN-BASHA <bashamaan0213@gmail.com>
2024-08-24 13:30:54 +0530

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Task2 {
     // program to check the given number is positive or negative
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner obj = new Scanner(System.in);
         System.out.println("Enter the value needed");
-        int n = scanner.nextInt();
+        int n = obj.nextInt();
         if (n>0)//checks the given number is positive or negative
         {
             System.out.println("Given number is positive.");
