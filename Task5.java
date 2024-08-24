2b5ec7f56ce34d2ec5315afe76b58e798c453965
AMAAN-BASHA <bashamaan0213@gmail.com>
2024-08-24 13:33:58 +0530

    Update Task5.java

--- Task5.java
+++ Task5.java
@@ -3,8 +3,8 @@ public class Task5{
     // program to calculate the final amount as per the discount
     public static void main(String [] args){
         System.out.println("Enter the purchase amount:");
-        Scanner s = new Scanner(System.in);
-        int purchaseAmount = s.nextInt();
+        Scanner obj = new Scanner(System.in);
+        int purchaseAmount = obj.nextInt();
         int discount=0;
         if ( purchaseAmount < 500)
         {
